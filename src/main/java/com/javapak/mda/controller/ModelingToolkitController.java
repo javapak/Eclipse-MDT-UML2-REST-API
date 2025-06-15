99a24c73d3e5fac762faef7f6dedb9f474a74ef1
James Pakis <japakis@memphis.edu>
2025-06-15 08:00:28 -0500

    correctly configures emf json jackson serialization

--- src/main/java/com/javapak/mda/controller/ModelingToolkitController.java
+++ src/main/java/com/javapak/mda/controller/ModelingToolkitController.java
@@ -10,7 +10,7 @@ import reactor.core.scheduler.Schedulers;
 
 @RestController
 @RequestMapping("/api/v1/toolkit")
-@CrossOrigin(origins = "http://localhost:3000") // temp local testing only... remove later!
+@CrossOrigin(origins = "http://localhost:5173") // temp local testing only... remove later!
 public class ModelingToolkitController {
 
     @Autowired
